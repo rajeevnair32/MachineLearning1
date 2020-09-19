a2909e94f8237f350a680803831046dc1435b654
Rajeev Nair <rnair64@gatech.edu>
2020-09-18 23:58:29 -0500

    updates

--- adult-income.py
+++ adult-income.py
@@ -23,50 +23,49 @@ adult_df = pd.read_csv('datasets/adult_processed.csv')
 FEATURES = ['marital-status', 'educational-num', 'relationship', 'age']
 fig_path = './figures/adult-income/'
 
-print("Find best parameters")
-# defining parameter range
-param_grid = {'C' : [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'kernel': ['linear', 'sigmoid', 'rbf']}
-find_best_param(SVC, 'income', FEATURES, adult_df, param_grid)
+def find_best():
 
-# defining parameter range
-param_grid = {'C' : [0.1, 1, 10, 100], 'loss': ['hinge', 'squared_hinge']}
-find_best_param(LinearSVC, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'C' : [0.01, 0.1, 0.5, 1], 'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'kernel': ['linear', 'rbf', 'sigmoid']}
+    find_best_param(SVC, 'income', FEATURES, adult_df, param_grid)
 
-# defining parameter range
-param_grid = {'activation' : ['identity', 'logistic', 'tanh', 'relu'], 'solver' : ['lbfgs','sgd','adam'], 'learning_rate': ['constant','invscaling','adaptive']}
-find_best_param(MLPClassifier, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'C' : [0.01, 0.1, 0.5, 1], 'loss': ['hinge', 'squared_hinge']}
+    find_best_param(LinearSVC, 'income', FEATURES, adult_df, param_grid)
 
-# defining parameter range
-param_grid = {'n_estimators': [20, 50, 100, 200, 500, 1000], 'learning_rate': [1, 0.1, 0.01, 0.001], 'algorithm' : ['SAMME', 'SAMME.R']}
-find_best_param(AdaBoostClassifier, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'activation' : ['identity', 'logistic', 'tanh', 'relu'], 'solver' : ['lbfgs','sgd','adam'], 'learning_rate': ['constant','invscaling','adaptive']}
+    find_best_param(MLPClassifier, 'income', FEATURES, adult_df, param_grid)
 
-# defining parameter range
-param_grid = {'loss': ['deviance', 'exponential'], 'criterion': ['friedman_mse', 'mse', 'mae'], 'n_estimators': [10, 20, 50, 100, 200], 'learning_rate': [1, 0.1, 0.01, 0.001],}
-find_best_param(GradientBoostingClassifier, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'n_estimators': [20, 50, 100, 200, 500, 1000], 'learning_rate': [1, 0.1, 0.01, 0.001], 'algorithm' : ['SAMME', 'SAMME.R']}
+    find_best_param(AdaBoostClassifier, 'income', FEATURES, adult_df, param_grid)
 
-# defining parameter range
-param_grid = {'criterion': ['gini', 'entropy'], 'max_depth': np.arange(1, 10, 1), 'min_samples_split': np.arange(2, 21, 1)}
-find_best_param(DecisionTreeClassifier, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'loss': ['deviance', 'exponential'], 'criterion': ['friedman_mse', 'mse', 'mae'], 'n_estimators': [10, 20, 50, 100, 200], 'learning_rate': [1, 0.1, 0.01, 0.001],}
+    find_best_param(GradientBoostingClassifier, 'income', FEATURES, adult_df, param_grid)
 
-# defining parameter range - using only odd numbers
-param_grid = {'n_neighbors': np.arange(1, 42, 2)}
-find_best_param(KNeighborsClassifier, 'income', FEATURES, adult_df, param_grid)
+    # defining parameter range
+    param_grid = {'criterion': ['gini', 'entropy'], 'max_depth': np.arange(1, 10, 1), 'min_samples_split': np.arange(2, 21, 1)}
+    find_best_param(DecisionTreeClassifier, 'income', FEATURES, adult_df, param_grid)
 
-print("Best parameters - end")
+    # defining parameter range - using only odd numbers
+    param_grid = {'n_neighbors': np.arange(1, 42, 2)}
+    find_best_param(KNeighborsClassifier, 'income', FEATURES, adult_df, param_grid)
 
-import sys
-sys.exit()
+# Need to run only once to get best params for respective classifier
+#find_best()
 
 result_dict = {
-               'income - kNearestNeighbors': build_model(knearest_neighbors_fn, 'income', FEATURES, adult_df),
-               'income - Linear SVM': build_model(linear_svm_fn, 'income', FEATURES, adult_df),
-               'income - SVM Linear': build_model(svm_linear_fn, 'income', FEATURES, adult_df),
-               'income - SVM Sigmoid': build_model(svm_sigmoid_fn, 'income', FEATURES, adult_df),
-               'income - SVM RBF': build_model(svm_rbf_fn, 'income', FEATURES, adult_df),
-               'income - Ada Boosting': build_model(ada_boosting_fn, 'income', FEATURES, adult_df),
-               'income - Gradient Boosting': build_model(gradient_boosting_fn, 'income', FEATURES, adult_df),
-               'income - Neural networks': build_model(neural_network_fn, 'income', FEATURES, adult_df),
-               'income - Decision_tree': build_model(decision_tree_fn, 'income', FEATURES, adult_df)
+               'income - kNearestNeighbors': build_model(knearest_neighbors_fn, 'income', FEATURES, adult_df, options={'k': 33}),
+               'income - Linear SVM': build_model(linear_svm_fn, 'income', FEATURES, adult_df, options={'C': 0.1, 'loss': 'squared_hinge'}),
+               'income - SVM Linear': build_model(svm_linear_fn, 'income', FEATURES, adult_df, options={'C': 0.1, 'gamma': 1}),
+               'income - SVM Sigmoid': build_model(svm_sigmoid_fn, 'income', FEATURES, adult_df, options={'C': 0.1, 'gamma': 1}),
+               'income - SVM RBF': build_model(svm_rbf_fn, 'income', FEATURES, adult_df, options={'C': 0.1, 'gamma': 1}),
+               'income - Ada Boosting': build_model(ada_boosting_fn, 'income', FEATURES, adult_df, options={'algorithm': 'SAMME.R', 'learning_rate': 1, 'n_estimators': 100}),
+               'income - Gradient Boosting': build_model(gradient_boosting_fn, 'income', FEATURES, adult_df, options={'criterion': 'friedman_mse', 'learning_rate': 0.1, 'loss': 'exponential', 'n_estimators': 100}),
+               'income - Neural networks': build_model(neural_network_fn, 'income', FEATURES, adult_df, options={'activation': 'identity', 'learning_rate': 'constant', 'solver': 'lbfgs'}),
+               'income - Decision_tree': build_model(decision_tree_fn, 'income', FEATURES, adult_df, options={'criterion': 'gini', 'max_depth': 3, 'min_samples_split': 2})
                }
 
 # Running code with default values
@@ -75,37 +74,37 @@ plt = print_results(result_dict)
 plt.savefig(fig_path + 'results.png')
 
 title = "Learning Curves for Decision Tree"
-plt = plot_learning_curve(DecisionTreeClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(DecisionTreeClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_dt.png')
 
 title = "Learning Curves for Neural Networks"
-plt = plot_learning_curve(MLPClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(MLPClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_neural.png')
 
 title = "Learning Curves for AdaBoost"
-plt = plot_learning_curve(AdaBoostClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(AdaBoostClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_adaboost.png')
 
 title = "Learning Curves for GradientBoost"
-plt = plot_learning_curve(GradientBoostingClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(GradientBoostingClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_gradientboost.png')
 
 title = "Learning Curves for KNeighbors"
-plt = plot_learning_curve(KNeighborsClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(KNeighborsClassifier(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_kneighbors.png')
 
 title = "Learning Curves for SVC"
-plt = plot_learning_curve(SVC(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(SVC(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_svc.png')
 
 title = "Learning Curves for LinearSVC"
-plt = plot_learning_curve(LinearSVC(), 'income', FEATURES, adult_df, title, ylim=(0.7, 1.01))
+plt = plot_learning_curve(LinearSVC(), 'income', FEATURES, adult_df, title, ylim=(0.4, 1.01))
 #plt.show()
 plt.savefig(fig_path + 'learning_curve_linearsvc.png')
 
