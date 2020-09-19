3101c80bb48b211362e067e21091d80a92be0efa
Rajeev Nair <rnair64@gatech.edu>
2020-09-18 22:17:19 -0500

    add files

--- helper.py
+++ helper.py
@@ -249,6 +249,7 @@ def linear_svm_fn(x_train, y_train, options={}):
                                   loss=options['loss'],
                                   tol=options['tol'])
     model.fit(x_train, y_train)
+    return model
 
 def svm_linear_classifier(C=1.0, max_iter=1000, tol=1e-3, gamma=1):
     return SVC(kernel='linear', C=C, max_iter=max_iter, tol=tol, gamma=gamma)
